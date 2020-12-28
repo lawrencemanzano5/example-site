f98a7064102d7e82c3cca1d9d0b308ab274277ec
Lawrence <lawrencemanzano5@gmail.com>
2020-12-27 23:03:54 -0700

    Added example code to test OOP JS principles.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,26 @@
 import '../styles/styles.css'
+// import Person from './modules/Person'
 
 if (module.hot) {
     module.hot.accept();
 }
+
+/* 
+//OOP example starts below:
+
+// new Adult class that inherits everything from the Person class
+class Adult extends Person {
+    payTaxes() {
+        console.log(this.name + " now owes zero taxes.");
+    }
+}
+
+// new instance of Person
+var john = new Person("John Doe", "purple");
+john.greet();
+
+// new instance of Adult
+var jane = new Adult("Jane Smith", "green");
+jane.greet();
+jane.payTaxes();
+*/
